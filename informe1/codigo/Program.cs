13c58683cbfa03149d94394999f9eb5fd4a2c1ee
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2026-05-12 18:32:18 -0500

    feat: add dotnet

---
+++ informe1/codigo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Programa
+{
+    public void Saludar()
+    {
+        Console.WriteLine("¡Hola a todos!");
+    }
+
+    static void Main(string[] args)
+    {
+        Programa p = new Programa();
+        p.Saludar();
+    }
+}
